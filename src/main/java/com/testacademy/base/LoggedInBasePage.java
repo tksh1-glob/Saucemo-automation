add794e829a2f04c9b9d67b76b78fef4ff014d93
tksh1-glob <gustavo.cabrera@globant.com>
2026-09-01 18:28:16 -0500

    feat:Procesos

--- src/main/java/com/testacademy/base/LoggedInBasePage.java
+++ src/main/java/com/testacademy/base/LoggedInBasePage.java
@@ -1,9 +1,44 @@
 package com.testacademy.base;
 
+import com.testacademy.pages.CartPage;
+import com.testacademy.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public abstract class LoggedInBasePage extends BasePage {
+
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement menuButton;
+
+    @FindBy(id = "logout_sidebar_link")
+    private WebElement logoutLink;
+
+    @FindBy(className = "shopping_cart_link")
+    private WebElement cartIcon;
+
+    @FindBy(className = "shopping_cart_badge")
+    private WebElement cartBadge;
+
     protected LoggedInBasePage(WebDriver driver) {
         super(driver);
+    }
+
+    public LoginPage logout() {
+        click(menuButton);
+        click(logoutLink);
+        return new LoginPage(driver);
+    }
+
+    public CartPage goToCart() {
+        click(cartIcon);
+        return new CartPage(driver);
+    }
+
+    public int getCartItemsCount() {
+        if (!isDisplayed(cartBadge)) {
+            return 0;
+        }
+        return Integer.parseInt(getText(cartBadge));
     }
 }
--- src/main/java/com/testacademy/pages/InventoryPage.java
+++ src/main/java/com/testacademy/pages/InventoryPage.java
@@ -1,10 +1,39 @@
 package com.testacademy.pages;
 
 import com.testacademy.base.LoggedInBasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.Random;
 
 public class InventoryPage extends LoggedInBasePage {
+
+    @FindBy(className = "inventory_item")
+    private List<WebElement> inventoryItems;
+
     public InventoryPage(WebDriver driver) {
         super(driver);
+    }
+
+    public InventoryPage addRandomProductToCart() {
+        int randomIndex = new Random().nextInt(inventoryItems.size());
+        return addProductToCartByIndex(randomIndex);
+    }
+
+    public InventoryPage addMultipleProductsToCart(int amount) {
+        for (int i = 0; i < amount && i < inventoryItems.size(); i++) {
+            addProductToCartByIndex(i);
+        }
+        return this;
+    }
+
+    private InventoryPage addProductToCartByIndex(int index) {
+        WebElement item = inventoryItems.get(index);
+        WebElement addToCartButton = item.findElement(By.cssSelector("button"));
+        click(addToCartButton);
+        return this;
     }
 }
